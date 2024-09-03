4eb937ff03d3488af5fd173d8b27975fb28624f4
Gavin Schroeder <gschroeder8@my.wctc.edu>
2024-09-02 21:43:41 -0500

    Submission

--- discount.js
+++ discount.js
@@ -2,8 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
   document.getElementById('discount-row').addEventListener('click', function(e){
     if (e.target.classList.contains('discount')) {
       e.preventDefault();
-      document.getElementById('product').innerHTML = e.target.dataset['product'];
+      document.getElementById('product').innerHTML = 'Discount Title: ' + e.target.dataset['product'];
+      document.getElementById('discount-code').innerHTML = 'Discount Code: ' + e.target.dataset['code'];
       bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+    }
+  });
+
+  const toastElement = document.getElementById('liveToast');
+  const toastInstance = bootstrap.Toast.getOrCreateInstance(toastElement);
+  
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+      toastInstance.hide();
     }
   });
 });
